b47c65785bc196748538bf1113185cb270a04557
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-25 13:51:23 -0700

    finished ToDoList final version I think?

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
+  const [inputText, setInputText] = React.useState(""); //keep inputText state variable in this module
+
+  function handleChange(event) {
+    //set value of inputText state variable
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <button
+        // call addItem in App.jsx module, passing the input text
+        onClick={() => {
+          props.addItem(inputText);
+          setInputText("");
+        }}
+      >
         <span>Add</span>
       </button>
     </div>
